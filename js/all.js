c45c84613971417b0a622a6bf0d9ca7e4711e4c5
kyonismis1 <kyonismis1@gmail.com>
2020-12-19 03:02:02 +0800

    up layout & mobile

--- js/all.js
+++ js/all.js
@@ -1,4 +1,26 @@
 $(document).ready(function () {
+    $('.menu > li > .slidemenu').click(function (event) {
+        event.preventDefault();
+        $(this).toggleClass('slideopen');
+        $(this).parent().siblings().find('a').removeClass('slideopen')
+        $(this).parent().find('ul').slideToggle();
+        $(this).parent().siblings().find('ul').slideUp();
+    });
+    $(".show_sidebar>a").on('click', function (e) {
+        e.preventDefault();
+        $('.menu,.navbar').toggleClass('open');    
+    });
     
-    
+
+
+
+
+
+
+
+
+
+
+
+
 });
